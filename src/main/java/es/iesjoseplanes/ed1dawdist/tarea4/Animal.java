69e8fd83a9feebc7c6d9e9076d367151adb2c7d5
nico <nico@192.168.18.41>
2025-03-17 10:53:53 +0100

    Aplicada refactorización al código: ocultación de atributos, constructores con nombre y método toString.

---
+++ src/main/java/es/iesjoseplanes/ed1dawdist/tarea4/Animal.java
@@ -0,0 +1,44 @@
+package es.iesjoseplanes.ed1dawdist.tarea4;
+
+public class Animal {
+
+    /**
+     * @return the nombre
+     */
+    public String getNombre() {
+        return nombre;
+    }
+
+    /**
+     * @param nombre the nombre to set
+     */
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    private String nombre;
+
+    public Animal(String nombre) {
+        this.nombre = nombre;
+    }
+
+    void comer() {
+        System.out.println("Necesita comer diarimente para sobrevivir");
+    }
+
+    void dormir() {
+        System.out.println("Dormir es indispensable para descansar");
+    }
+
+    void reproducir() {
+        System.out.println("Originar nuevos seres vivos");
+    }
+
+    void relacionar(Animal a) {
+        System.out.println("Con el animal: " + a.getNombre());
+    }
+
+    String ToString() {
+        return this.getNombre();
+    }
+}
